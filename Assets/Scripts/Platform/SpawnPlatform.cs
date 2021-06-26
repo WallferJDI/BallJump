131983c840fa763f14bcfa2ca93e1fadf123fb82
Wallfer <wallferjdi@gmail.com>
2021-06-27 02:37:28 +0300

    Добавил спавн шарика

--- Assets/Scripts/Platform/SpawnPlatform.cs
+++ Assets/Scripts/Platform/SpawnPlatform.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class SpawnPlatform : Platform
 {
+    [SerializeField] private Ball _ball;
+    [SerializeField] private Transform _spawnPoint;
 
+    private void Awake()
+    {
+        Instantiate(_ball, _spawnPoint.position, Quaternion.identity);
+    }
 }
